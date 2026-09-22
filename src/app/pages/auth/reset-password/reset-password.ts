c21e84573c9b0bc133f4807dee7aa96723ea6ca3
gerry404 <241924705+gerry404@users.noreply.github.com>
2026-09-22 06:27:46 +0200

    architecture

---
+++ src/app/pages/auth/reset-password/reset-password.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-reset-password',
+  styleUrl: './reset-password.scss',
+  templateUrl: './reset-password.html',
+})
+export class ResetPassword {}
---
+++ src/app/shared/components/input/input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-input',
+  styleUrl: './input.scss',
+  templateUrl: './input.html',
+})
+export class Input {}
